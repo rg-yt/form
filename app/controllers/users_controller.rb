06fd51e90f751715effd9d2d1df67ddeaa05232d
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-03-27 16:57:03 -0400

    Define new and create methods on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(username: params[:username], password: params[:password], email: params[:email])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
