a2fbcd89790a98841ce0be3ace2497f87d45b846
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-03-27 17:04:03 -0400

    Update user controller to accept the new user hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], password: params[:password], email: params[:email])
+    # @user = User.new(username: params[:username], password: params[:password], email: params[:email])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def user_params
+    params.expect(user: [:username, :password, :email])
   end
 end
