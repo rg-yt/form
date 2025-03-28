b9e66c76d465961fa8238efbc6272af01fdf8e5a
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-03-27 21:06:52 -0400

    Allow user to edit and adjust views to show errors on form

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "users#new"
-  resources :users, only: [ :new, :create ]
+  resources :users, only: [ :new, :create, :update, :edit ]
 end
